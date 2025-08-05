eafb8c1621b0b7705a858f22d0a22f68014270bf
douglascugliarisenai <douglas_s5@estudante.sc.senai.br>
2025-08-05 19:21:49 -0300

    feat: 🎸 Implementação Função Sortear
    Implementação da função para sortear o azarado sortudo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import carregarPessoas from "./function/carregar-pessoas";
+import sortearAzarado from "./function/sortear-azarado";
+import terminal from "./ui/terminal";
+
 
 const pessoas = carregarPessoas('src/data/dados.csv');
+const azarado = sortearAzarado(pessoas);
 
-pessoas.forEach(pessoa => {
-    console.log(pessoa.toString());
-});
+
+terminal.clear();
+
+terminal.white('\n\nO').brightRed(' azarado')
+terminal.bold.brightGreen(' mais sortudo do dia').white(' é: \n');
+terminal.bold.brightYellow(azarado.toString());
+terminal('\n\n 🎉🎉🎉🎉🎉🎉🎉');
+console.log('');
